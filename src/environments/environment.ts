ab3edd1583fd4af8b585b4221dd32f186d2f7252
stamayo <sebastian18t@outlook.com>
2022-06-09 18:25:55 -0500

    add: variables and models for the weather api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  weather: {
+    apiKey: "a02f512f14c42341eab84729ef331bc2", 
+    api:"https://api.openweathermap.org/data/2.5/weather"
+  }
 };
 
 /*
