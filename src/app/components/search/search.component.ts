c53a9664f59742678a9b2c577851830038c1cb5c
stamayo <sebastian18t@outlook.com>
2022-06-09 18:04:19 -0500

    feat: search input

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -7,10 +9,36 @@ import { Component, OnInit} from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  inputSearch = new FormControl(''); 
+  @Output() submitted = new EventEmitter<string>(); 
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.onChange();
   }
 
+  private onChange(): void {
+    /**
+     * Detectar los cambios que ocurran en el input, teniendo en cuenta
+     * 1. Que haya un valor en el input
+     * 2. Hay una espera de .6ms para enviar el contenido del input
+     */
+
+    this.inputSearch.valueChanges
+    .pipe(
+
+      map((search: string) => search.trim()), //Elimina los espacios en blanco  de los extremos
+      debounceTime(600),  // Espera .6s para emitir el valor actual
+      filter((search: string) => search !== '')
+    )
+    .subscribe((search) =>{
+
+      //Enviar la información al componente padre
+      this.submitted.emit(search);
+    });
+
+  }
 
 }
--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -12,4 +12,7 @@ export class WeatherComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSearch(search: string){
+    console.log(search);
+  }
 }
